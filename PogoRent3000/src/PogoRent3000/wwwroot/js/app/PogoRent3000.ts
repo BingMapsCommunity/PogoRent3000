3e1bb5ab249e1e48bb238342096d95a20a432b03
Per Fahén <perfahlen@gmail.com>
2016-11-24 09:53:06 +0100

    added init ts file

--- PogoRent3000/src/PogoRent3000/wwwroot/js/app/PogoRent3000.ts
+++ PogoRent3000/src/PogoRent3000/wwwroot/js/app/PogoRent3000.ts
@@ -15,7 +15,7 @@ module PogoRent3000 {
         private initMap(): void {
             let mapContainer = document.querySelector("map") as HTMLDivElement;
             this.map = new Microsoft.Maps.Map(mapContainer, {
-                credentials: {},
+                credentials: 'AsXOzwxphj5MnBu0JvpoF7joDb6BdaAa8NHUjUbHj-S9n-_1DzS3vTHfSVmVyXnn',
                 center: new Microsoft.Maps.Location(62.5, 17.2),
                 zoom: 10
             });
