aa48795bde69f43651c0b2a68d92e7920c470ac3
Per Fahén <perfahlen@gmail.com>
2016-11-24 22:48:32 +0100

    added geometry over gold coast

---
+++ PogoRent3000/src/PogoRent3000/wwwroot/lib/pogorent3000.js
@@ -0,0 +1,57 @@
+/// <reference path="definitions/microsoft.maps.d.ts" />
+/// <reference path="definitions/microsoft.maps.all.d.ts" />
+/// <reference path="definitions/modules/geojson.d.ts" />
+/// <reference path="definitions/whatwg-fetch.d.ts" />
+var PogoRent3000;
+(function (PogoRent3000) {
+    class Map {
+        constructor() {
+            this.loadBingMapModules();
+            this.initMap();
+            this.fetchGeoJson();
+        }
+        loadBingMapModules() {
+            Microsoft.Maps.loadModule("Microsoft.Maps.GeoJson");
+        }
+        initMap() {
+            let mapContainer = document.querySelector("#map");
+            mapContainer.style.width = "98vw";
+            mapContainer.style.height = "95vh";
+            this.map = new Microsoft.Maps.Map(mapContainer, {
+                credentials: '',
+                center: new Microsoft.Maps.Location(-28.014407569005286, 153.42029571533203),
+                zoom: 10
+            });
+            this.goldCoasLayer = new Microsoft.Maps.Layer("goldCoast");
+            this.map.layers.insert(this.goldCoasLayer);
+        }
+        parseGeoJSON(geoJson) {
+            if (!Microsoft.Maps.GeoJson) {
+                setTimeout(() => {
+                    this.parseGeoJSON(geoJson);
+                }, 100);
+            }
+            else {
+                let geoJSONtext = JSON.stringify(geoJson);
+                let primitives = Microsoft.Maps.GeoJson.read(geoJSONtext);
+                this.goldCoasLayer.add(primitives);
+            }
+        }
+        fetchGeoJson() {
+            var request = new Request("/api/GeoService/?service=wfs&version=1.0.0&request=GetFeature", {
+                method: 'GET', headers: new Headers({ 'Content-Type': 'text/plain' })
+            });
+            fetch(request).then((response) => {
+                return response.json(); // maybe just text ??? 
+            }).then((geojson) => {
+                this.parseGeoJSON(geojson);
+            });
+        }
+    }
+    PogoRent3000.Map = Map;
+})(PogoRent3000 || (PogoRent3000 = {}));
+document.onreadystatechange = () => {
+    if (document.readyState === "complete")
+        new PogoRent3000.Map();
+};
+//# sourceMappingURL=pogorent3000.js.map
---
+++ PogoRent3000/src/PogoRent3000/wwwroot/ts/app/PogoRent3000.ts
@@ -0,0 +1,66 @@
+﻿/// <reference path="definitions/microsoft.maps.d.ts" />
+/// <reference path="definitions/microsoft.maps.all.d.ts" />
+/// <reference path="definitions/modules/geojson.d.ts" />
+/// <reference path="definitions/whatwg-fetch.d.ts" />
+
+
+module PogoRent3000 {
+    export class Map {
+
+        private map: Microsoft.Maps.Map;
+        private goldCoasLayer: Microsoft.Maps.Layer;
+
+        constructor() {
+            this.loadBingMapModules();
+            this.initMap();
+            this.fetchGeoJson();
+        }
+
+        private loadBingMapModules(): void {
+            Microsoft.Maps.loadModule("Microsoft.Maps.GeoJson");
+        }
+
+        private initMap(): void {
+            let mapContainer = document.querySelector("#map") as HTMLDivElement;
+            mapContainer.style.width = "98vw";
+            mapContainer.style.height = "95vh";
+            this.map = new Microsoft.Maps.Map(mapContainer, {
+                credentials: '',
+                center: new Microsoft.Maps.Location(-28.014407569005286, 153.42029571533203),
+                zoom: 10
+            });
+            this.goldCoasLayer = new Microsoft.Maps.Layer("goldCoast");
+            this.map.layers.insert(this.goldCoasLayer);
+        }
+
+        private parseGeoJSON(geoJson: JSON): void {
+            if (!Microsoft.Maps.GeoJson) {
+                setTimeout(() => {
+                    this.parseGeoJSON(geoJson);
+                }, 100);
+            } else {
+                let geoJSONtext = JSON.stringify(geoJson);
+                let primitives = Microsoft.Maps.GeoJson.read(geoJSONtext) as Array<Microsoft.Maps.IPrimitive>;
+                this.goldCoasLayer.add(primitives);
+            }
+        }
+        
+
+        private fetchGeoJson(): void {
+            var request = new Request("/api/GeoService/?service=wfs&version=1.0.0&request=GetFeature", {
+                method: 'GET', headers: new Headers({ 'Content-Type': 'text/plain' })
+            });
+
+            fetch(request).then((response) => {
+                return response.json(); // maybe just text ??? 
+            }).then((geojson) => {
+                this.parseGeoJSON(geojson);
+            });
+        }
+    }
+}
+
+document.onreadystatechange = () => {
+    if (document.readyState === "complete")
+        new PogoRent3000.Map();
+}
